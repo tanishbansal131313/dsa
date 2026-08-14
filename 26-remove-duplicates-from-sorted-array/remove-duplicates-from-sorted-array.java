5c0403a51fbf6d94bf352d789fb4f06782e0d58a
tanish Bansal <tanishbansal1313@gmail.com>
2026-08-14 14:38:36 +0530

    Time: 1 ms (77.41%) | Memory: 47.1 MB (9.47%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int slow = 1;
+
+        for (int fast = 1; fast < nums.length; fast++) {
+
+            if (nums[fast] != nums[slow - 1]) {
+
+                nums[slow] = nums[fast];
+
+                slow++;
+            }
+        }
+
+        return slow;
+    }
+}
