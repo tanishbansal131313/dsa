27162edd86102037cda686d9e04bc8a4879af8a9
tanish Bansal <tanishbansal1313@gmail.com>
2026-08-24 22:31:02 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (51.73%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+
+        int i = s.length() - 1;
+
+        // Skip spaces at the end
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        int length = 0;
+
+        // Count the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            length++;
+            i--;
+        }
+
+        return length;
+    }
+}
