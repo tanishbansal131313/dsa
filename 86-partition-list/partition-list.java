60b45bb0ce772133f241f7b659a9bd62b40ebed1
tanish Bansal <tanishbansal1313@gmail.com>
2026-09-01 16:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (50.68%) - LeetSync

---
+++ 86-partition-list/partition-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+
+        ListNode lessDummy = new ListNode(0);
+        ListNode greaterDummy = new ListNode(0);
+
+        ListNode less = lessDummy;
+        ListNode greater = greaterDummy;
+
+        ListNode curr = head;
+
+        while (curr != null) {
+
+            if (curr.val < x) {
+                less.next = curr;
+                less = less.next;
+            } else {
+                greater.next = curr;
+                greater = greater.next;
+            }
+
+            curr = curr.next;
+        }
+
+        // End the greater/equal list
+        greater.next = null;
+
+        // Connect the two lists
+        less.next = greaterDummy.next;
+
+        return lessDummy.next;
+    }
+}
