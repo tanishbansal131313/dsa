b191f72404c09390207e2ef80c139e32810d1a44
tanish Bansal <tanishbansal1313@gmail.com>
2026-09-09 21:34:00 +0530

    Time: 1 ms (30.88%) | Memory: 45.1 MB (97.25%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if (Math.abs(left - right) > 1) {
+            return false;
+        }
+
+        return isBalanced(root.left) &&
+               isBalanced(root.right);
+    }
+
+    private int height(TreeNode root) {
+
+        if (root == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(
+            height(root.left),
+            height(root.right)
+        );
+    }
+}
